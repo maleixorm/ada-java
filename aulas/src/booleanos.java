89b2dcb03a7218d4eb1383ccdf3b9341cd5767d4
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-16 19:23:15 -0300

    Operadores booleanos

---
+++ aulas/src/booleanos.java
@@ -0,0 +1,9 @@
+public class booleanos {
+    public static void main(String[] args) {
+        // boolean resultado = false;
+        boolean fimDeSemana = false;
+        boolean fazendoSol = false;
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        System.out.println(vamosAPraia);
+    }
+}
