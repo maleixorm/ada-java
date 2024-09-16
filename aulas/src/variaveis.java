3fb517ed63ee124af01df1e2dbc6065c920581a3
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-16 17:46:23 -0300

    Variáveis e Casting no Java

---
+++ aulas/src/variaveis.java
@@ -0,0 +1,16 @@
+public class variaveis {
+    public static void main(String[] args) {
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}
