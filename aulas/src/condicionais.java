8f4030f4bdbb9620880b995f5d739b7660eb47d2
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-17 19:42:45 -0300

    Estruturas condicionais no Java

---
+++ aulas/src/condicionais.java
@@ -0,0 +1,13 @@
+public class condicionais {
+    public static void main(String[] args) {
+        int nota = 60;
+        int media = 70;
+
+        if (nota >= media) {
+            System.out.println("Aluno aprovado");
+        } else {
+            System.out.println("Aluno reprovado");
+        }
+    }
+
+}
